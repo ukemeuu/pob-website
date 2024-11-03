1a16c31d3ffd0668eeae4a6dc3614c1470ba92fa
“ukemeuu” <ukemeuuuu@gmail.com>
2024-11-03 17:40:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
