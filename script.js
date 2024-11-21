857677f1ff8cea80de55fd7379d166a43db7d697
“ukemeuu” <ukemeuuuu@gmail.com>
2024-11-21 22:25:48 +0300

    final code commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -10,3 +11,42 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("show-mobile-menu");
 });
+
+// Close menu when the nav link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click(), )
+})
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    passCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidePerView: 1,
+        },
+        768: {
+            slidePerView: 2,
+        },
+        1024: {
+            slidePerView: 3,
+        }
+    }
+  
+  });
